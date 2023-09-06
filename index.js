fa9800baed270d76183bfedddfe32c171f07b45a
Your Name <you@example.com>
2023-09-06 14:20:33 +0200

    modified:   index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function(req,res){
     res.writeHead(200,{'Content-Type': 'text/html'});
-    res.end('<h1>CHUJ 2</h1>');
+    res.end('<h1>CHUJ 2</h1>'+ req.socket.remoteAddress);
 }).listen(8080);
